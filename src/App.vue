a327db112ebe1de344a3e381e45c0299b2502093
MrNedNick <mrnediala@gmail.com>
2022-03-10 14:05:28 +0100

    Added an "About" page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
+
+    <v-navigation-drawer 
       v-model="drawer"
       app
-    >
-      <!--  -->
+      >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Vuetify Todo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Best Todo
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -14,13 +46,19 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
   export default {
-    data: () => ({ drawer: null }),
+    data: () => ({ 
+      drawer: null,
+      items: [
+          { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
+          { title: 'About', icon: 'mdi-help-box', to: '/about' },
+        ],
+    }),
   }
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio aperiam
+      fugit ipsam accusantium deleniti, minus unde, labore doloremque,
+      consequuntur aut tempore modi obcaecati vel blanditiis sapiente eos libero
+      dignissimos. Itaque!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio aperiam
+      fugit ipsam accusantium deleniti, minus unde, labore doloremque,
+      consequuntur aut tempore modi obcaecati vel blanditiis sapiente eos libero
+      dignissimos. Itaque!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio aperiam
+      fugit ipsam accusantium deleniti, minus unde, labore doloremque,
+      consequuntur aut tempore modi obcaecati vel blanditiis sapiente eos libero
+      dignissimos. Itaque!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio aperiam
+      fugit ipsam accusantium deleniti, minus unde, labore doloremque,
+      consequuntur aut tempore modi obcaecati vel blanditiis sapiente eos libero
+      dignissimos. Itaque!
+    </p>
   </div>
 </template>
