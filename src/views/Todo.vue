16095e3a92bedd8e736342b8b5e140ae96568057
MrNedNick <mrnediala@gmail.com>
2022-03-10 17:06:33 +0100

    Added todos

---
+++ src/views/Todo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <v-list flat>
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item>
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active" color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Wake up",
+        },
+        {
+          id: 2,
+          title: "Get bananas",
+        },
+        {
+          id: 3,
+          title: "Eat bananas",
+        },
+      ],
+    };
+  },
+};
+</script>
