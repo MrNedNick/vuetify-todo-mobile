7dc5b8a925af96994e3368a70c7c1c2db4a8a1c2
MrNedNick <mrnediala@gmail.com>
2022-03-22 15:57:24 +0100

    Add some features

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="home">
-    <v-list flat>
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable/>
+
+    <v-list flat class="pt-0">
       <div v-for="task in tasks" :key="task.id">
-        <v-list-item>
-          <template v-slot:default="{ active }">
+        <v-list-item
+        @click="doneTask(task.id)" :class="{ 'blue lighten-5' : task.done }">
+          <template v-slot:default>
             <v-list-item-action>
-              <v-checkbox :input-value="active" color="primary"></v-checkbox>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title :class="{ 'text-decoration-line-through' : task.done }">
+                {{ task.title }}
+              </v-list-item-title>
             </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn 
+                @click.stop="deleteTask(task.id)"
+                icon>
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+
           </template>
         </v-list-item>
         <v-divider></v-divider>
@@ -24,21 +47,43 @@ export default {
   name: "Home",
   data() {
     return {
+      newTaskTitle: '',
       tasks: [
         {
           id: 1,
           title: "Wake up",
+          done: false,
         },
         {
           id: 2,
           title: "Get bananas",
+          done: false,
         },
         {
           id: 3,
           title: "Eat bananas",
+          done: false,
         },
       ],
     };
   },
+  methods: {
+    addTask(){
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done:false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTitle = ''
+    },
+    doneTask(id){
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    },
+    deleteTask(id){
+      this.tasks = this.tasks.filter(task => task.id !== id)
+    }
+  }
 };
 </script>
