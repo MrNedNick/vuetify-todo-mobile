228c02acec24400b131649946e1b2b34fec0800d
MrNedNick <mrnediala@gmail.com>
2022-04-04 20:58:10 +0200

    Create Task child component

--- src/components/Todo/ListTasks.vue
+++ src/components/Todo/ListTasks.vue
@@ -1,54 +1,20 @@
 <template>
-    <v-list
-      class="pt-0"
-      flat
-    >
-      <div
-        v-for="task in $store.state.tasks"
-        :key="task.id"
-      >
-        <v-list-item
-          :class="{ 'blue lighten-5' : task.done }"
-          @click="$store.commit('doneTask', task.id)"
-        >
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox
-                :input-value="task.done"
-                color="primary"
-              />
-            </v-list-item-action>
-
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through' : task.done }"
-              >
-                {{ task.title }}
-              </v-list-item-title>
-            </v-list-item-content>
-
-            <v-list-item-action>
-              <v-btn 
-                @click.stop="$store.commit('deleteTask', task.id)"
-                icon
-              >
-                <v-icon color="primary lighten-1">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
-    </v-list>
+  <v-list class="pt-0" flat>
+    <task
+      v-for="task in $store.state.tasks"
+      :key="task.id"
+      :task="task"
+    />
+  </v-list>
 </template>
 
 <script>
 export default {
-
-}
+  components: {
+    task: require("@/components/Todo/Task.vue").default,
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/components/Todo/Task.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-list-item
+      :class="{ 'blue lighten-5': task.done }"
+      @click="$store.commit('doneTask', task.id)"
+    >
+      <template v-slot:default>
+        <v-list-item-action>
+          <v-checkbox :input-value="task.done" color="primary" />
+        </v-list-item-action>
+
+        <v-list-item-content>
+          <v-list-item-title
+            :class="{ 'text-decoration-line-through': task.done }"
+          >
+            {{ task.title }}
+          </v-list-item-title>
+        </v-list-item-content>
+
+        <v-list-item-action>
+          <v-btn @click.stop="$store.commit('deleteTask', task.id)" icon>
+            <v-icon color="primary lighten-1">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </template>
+    </v-list-item>
+    <v-divider></v-divider>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['task']
+};
+</script>
+
+<style>
+</style>
