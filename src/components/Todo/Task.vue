e79d0151140c91d0287194460d15d6bfb6095784
MrNedNick <mrnediala@gmail.com>
2022-04-05 11:06:55 +0200

    Create DialogDelete component

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -25,11 +25,16 @@
       </template>
     </v-list-item>
     <v-divider></v-divider>
+
+    <dialog-delete />
   </div>
 </template>
 
 <script>
 export default {
-    props: ['task']
+    props: ['task'],
+    components: {
+      'dialog-delete': require('@/components/Todo/Dialogs/DialogDelete.vue').default
+    }
 };
 </script>
