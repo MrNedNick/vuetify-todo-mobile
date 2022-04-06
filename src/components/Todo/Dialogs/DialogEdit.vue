d1bc49903a949f1e83c4d12f5703e48ab117a93f
MrNedNick <mrnediala@gmail.com>
2022-04-06 10:04:43 +0200

    Field validation

--- src/components/Todo/Dialogs/DialogEdit.vue
+++ src/components/Todo/Dialogs/DialogEdit.vue
@@ -25,6 +25,7 @@
           </v-btn>
           <v-btn
             color="red darken-1"
+            :disabled="taskTitleInvalid"
             text
             @click="saveTask"
           >
@@ -43,14 +44,21 @@ export default {
         taskTitle: null
       }
     },
+    computed: {
+      taskTitleInvalid() {
+        return !this.taskTitle || this.taskTitle === this.task.title
+      }
+    },
     methods: {
       saveTask() {
-        let payload = {
+        if (!this.taskTitleInvalid){
+          let payload = {
           id: this.task.id,
           title: this.taskTitle
         }
         this.$store.commit('updateTaskTitle', payload)
         this.$emit('close')
+        }
       }
     },
     mounted() {
