14ae5406eeb2e3ad7e6a990897aa234a82f912eb
MrNedNick <mrnediala@gmail.com>
2022-04-27 14:32:34 +0200

    Add tittle

--- src/components/Todo/TaskMenu.vue
+++ src/components/Todo/TaskMenu.vue
@@ -1,97 +1,88 @@
 <template>
-    <div>
-        <v-menu
-            bottom
-            left
-        >
-        <template
-            v-slot:activator="{ on, attrs }"
-        >
-        <v-btn
-            color="primary"
-            v-bind="attrs"
-            v-on="on"
-            icon
-        >
-            <v-icon>mdi-dots-vertical</v-icon>
+  <div>
+    <v-menu bottom left>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary" v-bind="attrs" v-on="on" icon>
+          <v-icon>mdi-dots-vertical</v-icon>
         </v-btn>
-        </template>
+      </template>
 
-        <v-list>
-            <v-list-item
-                v-for="(item, index) in items"
-                :key="index"
-                @click="handleClick(index)"
-            >
-            <v-list-item-icon>
-                <v-icon v-text="item.icon"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click="handleClick(index)"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-list-item>
-        </v-list>
+      </v-list>
     </v-menu>
-        <dialog-edit
-            v-if="dialogs.edit"
-            @close="dialogs.edit = false"
-            :task="task"
-        />
-        <dialog-due-date
-            v-if="dialogs.dueDate"
-            @close="dialogs.dueDate = false"
-            :task="task"
-        />
-        <dialog-delete
-            v-if="dialogs.delete"
-            @close="dialogs.delete = false"
-            :task="task"
-        />
-    </div>
-  
+    <dialog-edit
+      v-if="dialogs.edit"
+      @close="dialogs.edit = false"
+      :task="task"
+    />
+    <dialog-due-date
+      v-if="dialogs.dueDate"
+      @close="dialogs.dueDate = false"
+      :task="task"
+    />
+    <dialog-delete
+      v-if="dialogs.delete"
+      @close="dialogs.delete = false"
+      :task="task"
+    />
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['task'],
-    data: () => ({
-        dialogs: {
-          edit: false,
-          dueDate: false,
-          delete: false
-        },
-        items: [
-            { 
-                title: 'Edit',
-                icon: 'mdi-pencil',
-                click(){
-                    this.dialogs.edit = true
-                }
-            },
-            { 
-                title: 'Due date',
-                icon: 'mdi-calendar',
-                click(){
-                    this.dialogs.dueDate = true
-                } 
-            },
-            { 
-                title: 'Delete',
-                icon: 'mdi-delete',
-                click(){
-                    this.dialogs.delete = true
-                }
-            },
-        ],
-    }),
-    methods: {
-        handleClick(index){
-            this.items[index].click.call(this)
-        }
+  props: ["task"],
+  data: () => ({
+    dialogs: {
+      edit: false,
+      dueDate: false,
+      delete: false,
     },
-    components: {
-        'dialog-delete': require('@/components/Todo/Dialogs/DialogDelete.vue').default,
-        'dialog-edit': require('@/components/Todo/Dialogs/DialogEdit.vue').default,
-        'dialog-due-date': require('@/components/Todo/Dialogs/DialogDueDate.vue').default,
-    }
+    items: [
+      {
+        title: "Edit",
+        icon: "mdi-pencil",
+        click() {
+          this.dialogs.edit = true;
+        },
+      },
+      {
+        title: "Due date",
+        icon: "mdi-calendar",
+        click() {
+          this.dialogs.dueDate = true;
+        },
+      },
+      {
+        title: "Delete",
+        icon: "mdi-delete",
+        click() {
+          this.dialogs.delete = true;
+        },
+      },
+    ],
+  }),
+  methods: {
+    handleClick(index) {
+      this.items[index].click.call(this);
+    },
+  },
+  components: {
+    "dialog-delete": require("@/components/Todo/Dialogs/DialogDelete.vue")
+      .default,
+    "dialog-edit": require("@/components/Todo/Dialogs/DialogEdit.vue").default,
+    "dialog-due-date": require("@/components/Todo/Dialogs/DialogDueDate.vue")
+      .default,
+  },
 };
 </script>
 
